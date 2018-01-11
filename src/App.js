fa33a2fa76b2a38090b2ddaeec7e6a569bcfc303
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2018-01-11 15:42:58 -0600

    Update App component to dynamically build list of Persons based on state. Add functionality to delete Persons.

--- src/App.js
+++ src/App.js
@@ -13,16 +13,6 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 26 },
-      ]
-    });
-  }
-
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
@@ -31,6 +21,12 @@ class App extends Component {
         { name: 'Stephanie', age: 26 },
       ]
     });
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
   }
 
   togglePersonsHandler = () => {
@@ -53,17 +49,16 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div >
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, "Max!")}
-            changed={this.nameChangedHandler} />
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age} />
+          {
+            this.state.persons.map((person, index) => {
+              return (
+                <Person
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name}
+                  age={person.age} />
+              );
+            })
+          }
         </div>
       );
     }
