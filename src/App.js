609455981bfd3244b9834c03df8cd496bdc80ff7
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2018-01-12 09:03:10 -0600

    Update code to use CSS modules

--- src/App.js
+++ src/App.js
@@ -44,17 +44,8 @@ class App extends Component {
   }
 
   render() {
-
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      borderColor: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -74,7 +65,7 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
     const assignedClasses = [];
@@ -89,7 +80,7 @@ class App extends Component {
       <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
         <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>Switch Name</button>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>Switch Name</button>
         {persons}
       </div >
     );
