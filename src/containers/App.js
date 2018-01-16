eea811be5c436f783d46e69501286eb03b55999e
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2018-01-16 12:34:57 -0600

    Add PropTypes to validate input to Person component

--- src/containers/App.js
+++ src/containers/App.js
@@ -42,7 +42,8 @@ class App extends PureComponent {
       { id: 'asdfas', name: 'Stephanie', age: 26 }
     ],
     otherState: 'Some other value',
-    showPersons: false
+    showPersons: false,
+    toggleClicked: 0
   };
 
   nameChangedHandler = (event, id) => {
@@ -72,7 +73,12 @@ class App extends PureComponent {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
+    this.setState((prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      };
+    });
   };
 
   render() {
